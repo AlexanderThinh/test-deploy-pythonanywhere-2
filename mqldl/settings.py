b2dcb82c1516085e63f6016c71afd0d24c6f00ad
alexanderthinh <alexanderthinh@gmail.com>
2022-10-15 16:48:15 +0700

    Edit lan 2

--- mqldl/settings.py
+++ mqldl/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -154,3 +155,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Active Django-heroku
+django_heroku.settings(locals())
